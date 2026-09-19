67a98bdc011e5947696f09062af86c00c6370d92
Hussain <mmohammadhussain468@gmail.com>
2026-09-19 18:21:09 +0530

    day3-loops.py

---
+++ day3-loops.py
@@ -0,0 +1,52 @@
+#for loop
+for i in range(10):
+    print("Hello")
+
+#Start and end
+for i in range(5,9):
+    print(i)
+
+for i in range(9):
+    print(i)
+
+#for loop with calculation
+for i in range(1,10):
+    print(i*3)
+
+#while loop
+
+i=1
+while i<=7:
+    print(i)
+    i=i+1
+
+#p1
+for i in range(1,11):
+    print(i)
+
+#p2
+for i in range(10,0,-1):
+    print(i)
+
+#p3
+for i in range(2,11,2):
+    print(i)
+
+#p4
+i=1
+while i<=9:
+    print(i)
+    i=i+1
+
+#p5 break
+for i in range(1,9):
+    if i==5:
+        break
+    print(i)
+
+#p6 continue
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
+    
