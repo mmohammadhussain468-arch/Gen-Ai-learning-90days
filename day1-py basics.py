3654ecd8a70f76983439387c23516f2f071e2d85
mmohammadhussain468-arch <mmohammadhussain468@gmail.com>
2026-09-19 11:21:13 +0530

    Day 1: Python basics

---
+++ day1-py basics.py
@@ -0,0 +1,69 @@
+name="hussain"
+age=48.8
+details=('My age is',48)
+
+
+
+print('details:',details)
+print('name:',name)
+print('age:',age)
+
+print(type(name))
+print(type(age))
+print(type(details))
+
+
+price = 99.5
+print(type(price))
+
+is_student = True
+print(type(is_student))
+
+
+
+a=48
+b=68
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+
+name=input("enter a name:")
+print(name)
+print(type(name))
+
+
+
+
+#Homework
+#P1
+
+name=input("your name:")
+age=68
+city=input("your city:")
+print(name)
+print(age)
+print(city)
+
+
+#P2
+a = 20
+b = 10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#P3
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("Hello", name)
+print("You are", age, "years old")
+
+#p4
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print("Hello",name)
+print("Next year you will be", age + 1)
