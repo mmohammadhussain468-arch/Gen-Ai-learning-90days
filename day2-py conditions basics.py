1c681388e2b33d964e50dadada0b11cbcc7f5c9f
mmohammadhussain468-arch <mmohammadhussain468@gmail.com>
2026-09-19 12:44:51 +0530

    day2-py conditions basics.py

---
+++ day2-py conditions basics.py
@@ -0,0 +1,57 @@
+age=68
+if age > 70:
+    print("u are an adult")
+else:
+    print("u are not adult")
+
+
+age=int(input("Enter ur age:"))
+#if else
+if age>=18:
+    print("u eligible for vote")
+else:
+    print("not eligible for vote")
+
+#elif
+mark=86
+if mark >=90:
+    print("Excellent")
+elif mark >=48:
+    print("Good")
+elif mark >=44:
+    print("pass")
+else:
+    print("Fail")
+
+
+#p1
+age=20
+if age>=18:
+    print("Adult")
+else:
+    print("minor")
+
+#p2
+mark=int(input("Enter mark:"))
+if mark >=90:
+    print("ex")
+elif mark >=60:
+    print("ood")
+elif mark >=40:
+    print("ps")
+else:
+    print("fail")
+
+#p3
+age=int(input("ur age"))
+if age >=18:
+    print("adult")
+else:
+    print("minor")
+
+#p4
+number=int(input("enter num:"))
+if number >10:
+    print("Greater than 10")
+else:
+    print("less than 10")
