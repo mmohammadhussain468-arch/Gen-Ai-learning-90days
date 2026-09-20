512a733b92607749691c1fb48431ea0a757973f9
Hussain <mmohammadhussain468@gmail.com>
2026-09-20 22:51:40 +0530

    day4-lists.py

---
+++ day4-lists.py
@@ -0,0 +1,71 @@
+fruits=["orange","banana","apple","dragon fruit"]
+print(fruits[0])
+
+#change an item
+name=["ap",123,68.48,True]
+name[1]=468
+print(name)
+
+#Inedxing
+fruits = ["Apple", "Banana", "Mango"]
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+#add item
+name=["arul","andrew"]
+name.append("cogz")
+print(name)
+
+#remove item
+clik=["nan",123,]
+clik.remove(123)
+print(clik)
+
+#count items
+age=[12,15,17,19]
+print(len(age))
+
+#list+for loop
+fruits=["app","ora","dra"]
+for fruit in fruits:
+    print(fruit)
+
+#if+list
+age=[10,12,15,17,19,13]
+if 10 in age:
+    print("age is avail")
+
+
+#p1
+names=["hus","fat","sad"]
+print(names)
+
+#p2
+foods = ["Biryani", "Pizza", "Burger", "Dosa", "Fried Rice"]
+
+#p3
+foods = ["Biryani", "Pizza", "Burger", "Dosa", "Fried Rice"]
+foods.append("chilli")
+print(foods)
+
+#p4
+foods = ["Biryani", "Pizza", "Burger", "Dosa", "Fried Rice"]
+foods.remove("Pizza")
+print(foods)
+
+#p5
+foods = ["Biryani", "Pizza", "Burger"]
+for food in foods:
+    print(food)
+
+#p6
+numbers=[1,2,3,5,7,9,0]
+for number in numbers:
+    print(number)
+
+#p7
+fruits = ["Apple", "Banana", "Mango"]
+if "Banana" in fruits:
+    print("available")
